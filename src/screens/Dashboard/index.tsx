8a89791ad22961c359502af29f1263fc9bcdd4b7
Gabriel <gabriel.souza2@hotmail.com.br>
2022-03-16 22:19:42 -0300

    Estilizando Header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
 
-import { Container, Title } from './style';
+import { 
+    Container,
+    Header,
+    UserWrapper,
+    Photo,
+    User,
+    UserInfo,
+    UserGreatting,
+    UserName
+} from './style';
 
 export function Dashboard() {
     return (
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo source={{uri: "https://avatars.githubusercontent.com/u/19994168?s=400&u=ded5415ddc637e39f0d99d11163ee01c14ac757d&v=4"}}/>
+                        <User>
+                            <UserGreatting>Olá,</UserGreatting>
+                            <UserName>Gabriel</UserName>
+                        </User>
+                    </UserInfo>
+                </UserWrapper>
+            </Header>
         </Container>
     );
 }
