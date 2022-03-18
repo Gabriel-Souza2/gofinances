3ac30c9d54d5ef8486e3fa0631bc04aac5d9f36b
Gabriel <gabriel.souza2@hotmail.com.br>
2022-03-17 21:06:31 -0300

    posicionando Cartões de Destaques

--- src/screens/Dashboard/style.ts
+++ src/screens/Dashboard/style.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components/native';
 import { RFPercentage, RFValue } from "react-native-responsive-fontsize";
 import { Feather } from '@expo/vector-icons';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper'
 
 export const Container = styled.View`
     flex: 1;
@@ -11,9 +12,6 @@ export const Header = styled.View`
     width: 100%;
     height: ${RFPercentage(42)}px;
     background-color: ${({ theme }) => theme.colors.primary};
-
-    justify-content: center;
-    align-items: center;
     
 `;
 export const UserWrapper = styled.View`
@@ -22,6 +20,7 @@ export const UserWrapper = styled.View`
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
+    margin-top: ${getStatusBarHeight() + 28}px;
    
 `;
 
@@ -60,4 +59,7 @@ export const HighLightCards = styled.ScrollView.attrs({
     horizontal: true,
     showsHorizontalScrollIndicator: false,
     contentContainerStyle: {paddingHorizontal: 24}
-})``;
+})`
+    position: absolute;
+    margin-top: ${RFPercentage(20)}px;
+`;
