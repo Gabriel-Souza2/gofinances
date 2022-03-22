84921f029e1acefe87fd37e86a1e5c386ad7737f
Gabriel <gabriel.souza2@hotmail.com.br>
2022-03-21 23:33:21 -0300

    Iniciando Tela de Cadastro

--- App.tsx
+++ App.tsx
@@ -11,7 +11,7 @@ import {
 } from '@expo-google-fonts/poppins';
 import theme from './src/global/styles/theme';
 
-import { Dashboard } from './src/screens/Dashboard';
+import { Register } from './src/screens/Register';
 
 
 export default function App() {
@@ -26,7 +26,7 @@ export default function App() {
   }
   return (
     <ThemeProvider theme={theme}>
-      <Dashboard></Dashboard>
+      <Register></Register>
       <StatusBar style="auto" />
     </ThemeProvider>
     
